984459a0a209eedb29030f58ac2158aa46aa736b
wasimm <wasim_40@yahoo.com>
2021-01-04 16:12:59 +0500

    first three skips of household characteristics

--- app/src/main/java/edu/aku/hassannaqvi/naunehal/ui/sections/Section08SEActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/naunehal/ui/sections/Section08SEActivity.java
@@ -2,11 +2,15 @@ package edu.aku.hassannaqvi.naunehal.ui.sections;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+
 import edu.aku.hassannaqvi.naunehal.R;
 import edu.aku.hassannaqvi.naunehal.core.MainApp;
 import edu.aku.hassannaqvi.naunehal.databinding.ActivitySection08seBinding;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.validatorcrawler.aliazaz.Clear;
 
 public class Section08SEActivity extends AppCompatActivity {
 
@@ -20,6 +24,40 @@ public class Section08SEActivity extends AppCompatActivity {
 
         bi = DataBindingUtil.setContentView(this, R.layout.activity_section_08se);
         bi.setForm(MainApp.form);
-//        setupSkips();
+        setupSkips();
+    }
+
+    private void setupSkips() {
+
+        // se05
+        bi.se05.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == bi.se0501.getId() || checkedId == bi.se0503.getId() || checkedId == bi.se0596.getId()) {
+                Clear.clearAllFields(bi.fldGrpCVse06);
+                bi.fldGrpCVse06.setVisibility(View.GONE);
+            } else {
+                bi.fldGrpCVse06.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+        // se09
+        bi.se09.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == bi.se0902.getId()) {
+                Clear.clearAllFields(bi.fldGrpCVse10);
+                bi.fldGrpCVse10.setVisibility(View.GONE);
+            } else {
+                bi.fldGrpCVse10.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // se16
+        bi.se16.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == bi.se1602.getId()) {
+                Clear.clearAllFields(bi.fldGrpCVse17);
+                bi.fldGrpCVse17.setVisibility(View.GONE);
+            } else {
+                bi.fldGrpCVse17.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
