2b3af0ec29d460ad202b44135529469bbbc56be5
SAJID LATIF <indus1530@gmail.com>
2021-01-04 15:59:25 +0500

    Section07CVActivity skips

--- app/src/main/java/edu/aku/hassannaqvi/naunehal/ui/sections/Section07CVActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/naunehal/ui/sections/Section07CVActivity.java
@@ -1,26 +1,75 @@
 package edu.aku.hassannaqvi.naunehal.ui.sections;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+
+import com.validatorcrawler.aliazaz.Clear;
+import com.validatorcrawler.aliazaz.Validator;
+
+import org.jetbrains.annotations.NotNull;
+
 import edu.aku.hassannaqvi.naunehal.R;
 import edu.aku.hassannaqvi.naunehal.core.MainApp;
 import edu.aku.hassannaqvi.naunehal.databinding.ActivitySection07cvBinding;
-
-import android.os.Bundle;
+import edu.aku.hassannaqvi.naunehal.ui.MainActivity;
 
 public class Section07CVActivity extends AppCompatActivity {
 
+    ActivitySection07cvBinding bi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        ActivitySection07cvBinding bi;
-
         super.onCreate(savedInstanceState);
-        // only in First Section
-        //MainApp.form = new Form();
-
         bi = DataBindingUtil.setContentView(this, R.layout.activity_section_07cv);
         bi.setForm(MainApp.form);
-//        setupSkips();
+        setupSkips();
+
     }
+
+
+    private void setupSkips() {
+        rgListener(bi.cv01, bi.cv0102, bi.llcv01);
+        rgListener(bi.cv11, bi.cv1102, bi.fldGrpCVcv12);
+        rgListener(bi.cv17, bi.cv1702, bi.fldGrpCVcv18);
+    }
+
+
+    private void rgListener(@NotNull RadioGroup rg, RadioButton rb, ViewGroup vg) {
+        rg.setOnCheckedChangeListener((radioGroup, i) -> {
+            Clear.clearAllFields(vg);
+            vg.setVisibility(View.VISIBLE);
+            if (i == rb.getId()) vg.setVisibility(View.GONE);
+        });
+    }
+
+
+    public void BtnContinue() {
+        if (!formValidation()) return;
+
+        // SaveDraft(); //<== This function is no longer needed after DataBinding
+        if (/*UpdateDB()*/ true) {
+            finish();
+            startActivity(new Intent(this, Section08SEActivity.class));
+        }
+    }
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
+
+    }
+
+
+    public void BtnEnd() {
+        finish();
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+
 }
