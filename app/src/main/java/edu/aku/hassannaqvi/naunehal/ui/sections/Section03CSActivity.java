3f8d272f5d9fb54bb0fb77318d2c732fe61b490a
SAJID LATIF <indus1530@gmail.com>
2021-01-04 13:05:30 +0500

    Section03CSActivity skips

--- app/src/main/java/edu/aku/hassannaqvi/naunehal/ui/sections/Section03CSActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/naunehal/ui/sections/Section03CSActivity.java
@@ -5,8 +5,15 @@ import androidx.databinding.DataBindingUtil;
 import edu.aku.hassannaqvi.naunehal.R;
 import edu.aku.hassannaqvi.naunehal.core.MainApp;
 import edu.aku.hassannaqvi.naunehal.databinding.ActivitySection03csBinding;
+import edu.aku.hassannaqvi.naunehal.ui.MainActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+
+import com.validatorcrawler.aliazaz.Clear;
+import com.validatorcrawler.aliazaz.Validator;
 
 public class Section03CSActivity extends AppCompatActivity {
 
@@ -20,6 +27,93 @@ public class Section03CSActivity extends AppCompatActivity {
 
         bi = DataBindingUtil.setContentView(this, R.layout.activity_section_03cs);
         bi.setForm(MainApp.form);
-//        setupSkips();
+        setupSkips();
+
     }
+
+    private void setupSkips() {
+
+        bi.cs03.setOnCheckedChangeListener((radioGroup, i) -> {
+            Clear.clearAllFields(bi.llcs03);
+            bi.llcs03.setVisibility(View.VISIBLE);
+            if (i == bi.cs0302.getId()) {
+                bi.llcs03.setVisibility(View.GONE);
+            }
+        });
+
+        bi.cs06.setOnCheckedChangeListener((radioGroup, i) -> {
+            Clear.clearAllFields(bi.fldGrpCVcs07);
+            Clear.clearAllFields(bi.fldGrpCVcs08);
+            Clear.clearAllFields(bi.fldGrpCVcs10);
+            Clear.clearAllFields(bi.fldGrpCVcs11);
+            bi.fldGrpCVcs07.setVisibility(View.VISIBLE);
+            bi.fldGrpCVcs08.setVisibility(View.VISIBLE);
+            bi.fldGrpCVcs10.setVisibility(View.VISIBLE);
+            bi.fldGrpCVcs11.setVisibility(View.VISIBLE);
+            if (i == bi.cs0602.getId()) {
+                bi.fldGrpCVcs07.setVisibility(View.GONE);
+                bi.fldGrpCVcs08.setVisibility(View.GONE);
+            } else if (i == bi.cs0601.getId()) {
+                bi.fldGrpCVcs10.setVisibility(View.GONE);
+                bi.fldGrpCVcs11.setVisibility(View.GONE);
+            }
+        });
+
+
+        radioGroup(bi.cs12);
+        radioGroup(bi.cs13);
+        radioGroup(bi.cs14);
+    }
+
+
+    public void radioGroup(RadioGroup grp) {
+        grp.setOnCheckedChangeListener(((radioGroup, i) -> {
+            Clear.clearAllFields(bi.fldGrpCVcs15);
+            Clear.clearAllFields(bi.fldGrpCVcs16);
+            Clear.clearAllFields(bi.fldGrpCVcs17);
+            Clear.clearAllFields(bi.fldGrpCVcs18);
+            Clear.clearAllFields(bi.fldGrpCVcs19);
+            bi.fldGrpCVcs15.setVisibility(View.VISIBLE);
+            bi.fldGrpCVcs16.setVisibility(View.VISIBLE);
+            bi.fldGrpCVcs17.setVisibility(View.VISIBLE);
+            bi.fldGrpCVcs18.setVisibility(View.VISIBLE);
+            bi.fldGrpCVcs19.setVisibility(View.VISIBLE);
+            if (bi.cs1202.isChecked()
+                    && bi.cs1302.isChecked()
+                    && bi.cs1402.isChecked()) {
+                bi.fldGrpCVcs15.setVisibility(View.GONE);
+                bi.fldGrpCVcs16.setVisibility(View.GONE);
+                bi.fldGrpCVcs17.setVisibility(View.GONE);
+                bi.fldGrpCVcs18.setVisibility(View.GONE);
+                bi.fldGrpCVcs19.setVisibility(View.GONE);
+            } if (i == bi.cs1402.getId()) {
+                bi.fldGrpCVcs15.setVisibility(View.GONE);
+            }
+        }));
+
+    }
+
+
+    public void BtnContinue() {
+        if (!formValidation()) return;
+
+        // SaveDraft(); //<== This function is no longer needed after DataBinding
+        if (/*UpdateDB()*/ true) {
+            finish();
+            startActivity(new Intent(this, Section04IMActivity.class));
+        }
+    }
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
+
+    }
+
+
+    public void BtnEnd() {
+        finish();
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+
 }
