fa848dd10c894e83cc763f58abde95c9dad1c538
SAJID LATIF <indus1530@gmail.com>
2021-01-05 10:08:11 +0500

    UsersModel

--- app/src/main/java/edu/aku/hassannaqvi/naunehal/models/Users.java
+++ app/src/main/java/edu/aku/hassannaqvi/naunehal/models/Users.java
@@ -1,5 +1,12 @@
 package edu.aku.hassannaqvi.naunehal.models;
 
+import android.database.Cursor;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.aku.hassannaqvi.naunehal.contracts.UsersContract;
+
 /**
  * Created by hassan.naqvi on 11/30/2016.
  */
@@ -11,6 +18,7 @@ public class Users {
     String password;
     String fullName;
 
+
     public String getUsername() {
         return username;
     }
@@ -19,6 +27,7 @@ public class Users {
         this.username = username;
         return this;
     }
+
 
     public String getPassword() {
         return password;
@@ -29,6 +38,7 @@ public class Users {
         return this;
     }
 
+
     public String getFullName() {
         return fullName;
     }
@@ -37,4 +47,31 @@ public class Users {
         this.fullName = fullName;
         return this;
     }
+
+
+    public Users Sync(JSONObject jsonObject) throws JSONException {
+        this.username = jsonObject.getString(UsersContract.UsersTable.COLUMN_USERNAME);
+        this.password = jsonObject.getString(UsersContract.UsersTable.COLUMN_PASSWORD);
+        this.fullName = jsonObject.getString(UsersContract.UsersTable.COLUMN_FULL_NAME);
+        return this;
+    }
+
+    public Users Hydrate(Cursor cursor) {
+        this.username = cursor.getString(cursor.getColumnIndex(UsersContract.UsersTable.COLUMN_USERNAME));
+        this.password = cursor.getString(cursor.getColumnIndex(UsersContract.UsersTable.COLUMN_PASSWORD));
+        this.fullName = cursor.getString(cursor.getColumnIndex(UsersContract.UsersTable.COLUMN_FULL_NAME));
+        return this;
+
+    }
+
+
+    public JSONObject toJSONObject() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put(UsersContract.UsersTable.COLUMN_USERNAME, this.username == null ? JSONObject.NULL : this.username);
+        json.put(UsersContract.UsersTable.COLUMN_PASSWORD, this.password == null ? JSONObject.NULL : this.password);
+        json.put(UsersContract.UsersTable.COLUMN_FULL_NAME, this.fullName == null ? JSONObject.NULL : this.fullName);
+        return json;
+    }
+
+
 }
