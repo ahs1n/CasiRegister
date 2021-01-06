6ddc1fa279905bba54730ef27bd747a2cf57981a
SAJID LATIF <indus1530@gmail.com>
2021-01-06 11:24:56 +0500

    Section082SEActivity Added

---
+++ app/src/main/java/edu/aku/hassannaqvi/naunehal/ui/sections/Section082SEActivity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.naunehal.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.naunehal.R;
+
+public class Section082SEActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_082se);
+    }
+}
